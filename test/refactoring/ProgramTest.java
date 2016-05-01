be9068716a54a845590fd3411f727da1ff0ba559
maddin-911 <martinmssner@yahoo.de>
2016-05-01 14:20:02 +0200

    Updated Test Files

--- test/refactoring/ProgramTest.java
+++ test/refactoring/ProgramTest.java
@@ -1,3 +1,5 @@
+package refactoring;
+
 import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,27 +12,11 @@ public class ProgramTest {
     
     public ProgramTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
     @Test
     public void testMain() {
         String result;
-        String expected="123";
+        String expected;
         Movie m1 = new Movie("movie1", 1);
         Movie m2 = new Movie("movie2", 2);
         Rental r1 = new Rental(m1, 10);
@@ -38,8 +24,13 @@ public class ProgramTest {
         Customer c1 = new Customer("joe");
         c1.addRental(r1);   c1.addRental(r2);   
         result = c1.statement();
-        result = "123";
-        Assert.assertEquals(expected, result);
+        expected = "Rental Record for joe\n" +
+"	Title		Days	Amount\n" +
+"	movie1		10	30.0\n" +
+"	movie2		5	4.5\n" +
+"Amount owed is 34.5\n" +
+"You earned 3 frequent renter points";
+        assertEquals(expected, result);
     }
     
 }
