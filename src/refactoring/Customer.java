15a5a80901fd0647c29961fc9b2b7457ef823c78
maddin-911 <martinmssner@yahoo.de>
2016-05-01 14:41:20 +0200

    getFrequentRenterPoints to Rental

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -26,7 +26,7 @@ class Customer {
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getAmount()) + "\n";
-        result += "You earned " + String.valueOf(getFrequentRenterPoints()) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
     
@@ -39,19 +39,16 @@ class Customer {
             //determine amounts for each line
             thisAmount = each.amountFor(each);
             //show figures for this rental
-             total += thisAmount;
+            total += thisAmount;
         }
         return total;
     }
-    private int getFrequentRenterPoints(){
+    private int getTotalFrequentRenterPoints(){
         int total = 0;
         Enumeration enum_rentals = rentals.elements();	        
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
-            total ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                total ++;
+            total+=each.getFrequentRenterPoints(each);
         }
         return total;
     }
