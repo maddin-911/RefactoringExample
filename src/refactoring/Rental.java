d9a78d18af08237a80aab16702a2fb375c6108b2
maddin-911 <martinmssner@yahoo.de>
2016-05-01 14:23:40 +0200

    added AmountFor Class

--- src/refactoring/Rental.java
+++ src/refactoring/Rental.java
@@ -1,3 +1,4 @@
+package refactoring;
 class Rental {
     private Movie movie;
     private int daysRented;
@@ -10,5 +11,24 @@ class Rental {
     }
     public Movie getMovie() {
         return movie;
+    }
+    public double amountFor(Rental each) {
+        double thisAmount = 0;
+        switch (each.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (each.getDaysRented() > 2)
+                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += each.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (each.getDaysRented() > 3)
+                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
